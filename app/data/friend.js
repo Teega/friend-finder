c0d86f2767305922267222228b53359174f42fda
Teega <ricardo.ortega79@gmail.com>
2018-03-04 14:00:21 -0700

    working friend finder

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Bryan Cranston',
+    profilePic: 'http://cdn-static.denofgeek.com/sites/denofgeek/files/2017/10/cranston.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
